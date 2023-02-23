33b6785021a9557d29c852712a36592ef123d296
swaglord2021 <nuoya1996@gmail.com>
2023-02-23 14:51:11 -0500

    updates to text content.

--- portfolio/projectData.js
+++ portfolio/projectData.js
@@ -1,7 +1,7 @@
 const projectArr = [
     {
         name: 'Chat App',
-        description: 'A fitness tool that calculates and tracks calories from activities including metabolic energy spend',
+        description: 'A chat application that serves bi-directional communications with user authentication',
         tools: ['React.js', 'Node.js'],
         link: 'https://chat-app-zs9s.onrender.com'
     },
@@ -12,8 +12,8 @@ const projectArr = [
         link: 'https://exercise-tracker-wor9.onrender.com'
     },
     {
-        name: 'Music Player',
-        description: 'A player powered by Spotify API',
+        name: 'Audio Player',
+        description: "A song player integrated with Spotify's library" ,
         tools: ['React.js', 'Node.js'],
         link: 'https://music-player-eigq.onrender.com'
     },
@@ -25,31 +25,31 @@ const projectArr = [
     },
     {
         name: 'Word Scramble',
-        description: 'A search tool that allows you to look up nutrients associated with any food or ingredient list',
+        description: 'A mini-game to guess the unscrambled sentence with points awarded for each sentence guessed correctly',
         tools: ['React.js'],
         link: 'https://guess-the-sentence.netlify.app'
     },
     {
         name: 'Kios Presentation',
-        description: 'A search tool that allows you to look up nutrients associated with any food or ingredient list',
+        description: 'A sample presentation used at a medical conferrence',
         tools: ['HTML', 'CSS', 'Javascript'],
         link: 'https://kios-presentation.netlify.app'
     },
     {
         name: 'Student Profile',
-        description: 'A search tool that allows you to look up nutrients associated with any food or ingredient list',
+        description: 'A sample directory that displays student infomration',
         tools: ['React.js'],
         link: 'https://student-grades.netlify.app'    
     },
     {
         name: 'Ecommerce Shop',
-        description: 'A search tool that allows you to look up nutrients associated with any food or ingredient list',
+        description: 'A sample shopping page for eccommerce products',
         tools: ['HTML', 'CSS', 'Javascript'],
         link: 'https://ecom-shoping-page.netlify.app'
     },
     {
         name: 'Calculator',
-        description: 'A search tool that allows you to look up nutrients associated with any food or ingredient list',
+        description: 'A simple calculator with basic operations',
         tools: ['HTML', 'CSS', 'Javascript'],
         link: 'https://basic-jscalculator.netlify.app'
     },
